7386b9a80b7e7309a42443c94dcc97c59e55970a
Mihai Crăiță <mcraita@gmail.com>
2013-05-01 22:43:49 +0300

    previous funcionality

--- js/simple-slide.js
+++ js/simple-slide.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
     var simple_slide = {};
+$(document).ready(function() {
     simple_slide.slides = $('.slide');
-    simple_slide.index = 0;
     simple_slide.width = $('.slide').width();
     simple_slide.height = $('.slide').height();
-    simple_slide.delay = 2000;
+    simple_slide.index = 0;
+    simple_slide.delay = 3000;
     
     simple_slide.slides.wrapAll('<div class="slideWrap" />');
 
@@ -16,7 +16,7 @@ $(document).ready(function() {
         $(this).css({'z-index':simple_slide.slides.length - index,'position':'absolute'});
     });
 
-    simple_slide.next = function()
+    simple_slide.next_slide = function()
     {
         if(this.index == this.slides.length-1)
         {
@@ -30,9 +30,46 @@ $(document).ready(function() {
         }
         this.slides.eq(this.index).animate({"left":"-="+simple_slide.width+"px"},"slow");
         this.index ++;
-        setTimeout( function() { simple_slide.next() },simple_slide.delay );
     }
 
-    setTimeout( function() { simple_slide.next() },simple_slide.delay );
+    simple_slide.previous_slide = function()
+    {
+        if(this.index == this.slides.length)
+        {
+            this.slides.css({'left':'-'+simple_slide.width+"px"});
+            this.slides.eq(this.index).css('left','0');
+            this.slides.eq(0).css({'left':'0','z-index':'0'});
+        }
+        if(this.index == 1)
+        {
+            this.slides.eq(0).css('z-index', simple_slide.slides.length);
+            this.slides.eq(0).css({'left':'-'+simple_slide.width+"px"});
+        }
+        if(this.index == 0)
+        {
+            this.slides.css({'left':'-'+simple_slide.width+"px"});
+            this.slides.eq(this.index).css({'left':'0','z-index':'0'});
+            this.index = this.slides.length;
+        }
+        this.index --;
+        this.slides.eq(this.index).animate({"left":"+="+simple_slide.width+"px"},"slow");
+    }
+
+    simple_slide.previous = function()
+    {
+        clearInterval(this.timer);
+        this.previous_slide();
+        this.timer = setInterval( function() { simple_slide.next() },simple_slide.delay );
+    }
+
+    simple_slide.next = function()
+    {
+        clearInterval(this.timer);
+        this.next_slide();
+        this.timer = setInterval( function() { simple_slide.next() },simple_slide.delay );
+    }
+
+    if(simple_slide.slides.length > 1)
+        simple_slide.timer = setInterval( function() { simple_slide.next() },simple_slide.delay );
 
 });
