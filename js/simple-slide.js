2d443ccd998c2da2f1c96c7c30fd77f47f19d908
Mihai Crăiță <mcraita@gmail.com>
2013-08-22 12:41:22 +0300

    Multiple slides on page now available

--- js/simple-slide.js
+++ js/simple-slide.js
@@ -1,75 +1,85 @@
     var simple_slide = {};
 $(document).ready(function() {
-    simple_slide.slides = $('.slide');
-    simple_slide.width = $('.slide').width();
-    simple_slide.height = $('.slide').height();
-    simple_slide.index = 0;
-    simple_slide.delay = 3000;
-    
-    simple_slide.slides.wrapAll('<div class="slideWrap" />');
+    simple_slide.slider = $('.slider');
+    simple_slide.slider.each( function ( index )
+    {
+        simple_slide.slider[index].slides = $(this).find('.slide'); 
+        simple_slide.slider[index].nb_of_slides = simple_slide.slider[index].slides.length; 
+        simple_slide.slider[index].width = simple_slide.slider[index].slides.width();
+        simple_slide.slider[index].height = simple_slide.slider[index].slides.height();
+        simple_slide.slider[index].index = 0;
+        simple_slide.slider[index].delay = 2000;
+        simple_slide.slider[index].slides.wrapAll('<div class="slideWrap" />');
 
-    $('.slideWrap').height(simple_slide.height)
-    .width(simple_slide.width)
-    .css({'position':'relative','overflow':'hidden'});
+        simple_slide.slider[index].wrap = $(this).find('.slideWrap');
 
-    $('.slide').each( function(index) {
-        $(this).css({'z-index':simple_slide.slides.length - index,'position':'absolute'});
+        simple_slide.slider[index].wrap.height(simple_slide.slider[index].height) .width(simple_slide.slider[index].width) .css({'position':'relative','overflow':'hidden'});
+        
+        simple_slide.slider[index].slides.each( function(slide_index) {
+            $(this).css({'z-index':simple_slide.slider[index].nb_of_slides - slide_index,'position':'absolute'});
+        });
+
+
+        simple_slide.slider[index].next_slide = function()
+        {
+            if(simple_slide.slider[index].index == simple_slide.slider[index].nb_of_slides-1)
+            {
+                simple_slide.slider[index].slides.eq(0).css({'left':'0','z-index':'0'});
+            }
+            if(simple_slide.slider[index].index == simple_slide.slider[index].nb_of_slides)
+            {
+                simple_slide.slider[index].index = 0;
+                simple_slide.slider[index].slides.css('left','0');
+                simple_slide.slider[index].slides.eq(simple_slide.slider[index].index).css('z-index', simple_slide.slider[index].nb_of_slides);
+            }
+            simple_slide.slider[index].slides.eq(simple_slide.slider[index].index).animate({"left":"-="+simple_slide.slider[index].width+"px"},"slow");
+            simple_slide.slider[index].index ++;
+        }
+
+        simple_slide.slider[index].previous_slide = function()
+        {
+            if(simple_slide.slider[index].index == simple_slide.slider[index].nb_of_slides)
+            {
+                simple_slide.slider[index].slides.css({'left':'-'+simple_slide.slider[index].width+"px"});
+                simple_slide.slider[index].slides.eq(simple_slide.slider[index].index).css('left','0');
+                simple_slide.slider[index].slides.eq(0).css({'left':'0','z-index':'0'});
+            }
+            if(simple_slide.slider[index].index == 1)
+            {
+                simple_slide.slider[index].slides.eq(0).css('z-index', simple_slide.slider[index].nb_of_slides);
+                simple_slide.slider[index].slides.eq(0).css({'left':'-'+simple_slide.slider[index].width+"px"});
+            }
+            if(simple_slide.slider[index].index == 0)
+            {
+                simple_slide.slider[index].slides.css({'left':'-'+simple_slide.slider[index].width+"px"});
+                simple_slide.slider[index].slides.eq(simple_slide.slider[index].index).css({'left':'0','z-index':'0'});
+                simple_slide.slider[index].index = simple_slide.slider[index].slides.length;
+            }
+            simple_slide.slider[index].index --;
+            simple_slide.slider[index].slides.eq(simple_slide.slider[index].index).animate({"left":"+="+simple_slide.slider[index].width+"px"},"slow");
+        }
+
+        simple_slide.slider[index].previous = function()
+        {
+            clearInterval(simple_slide.slider[index].timer);
+            simple_slide.slider[index].previous_slide();
+            simple_slide.slider[index].timer = setInterval( function() { simple_slide.slider[index].next() },simple_slide.slider[index].delay );
+        }
+
+        simple_slide.slider[index].next = function()
+        {
+            clearInterval(simple_slide.slider[index].timer);
+            simple_slide.slider[index].next_slide();
+            simple_slide.slider[index].timer = setInterval( function() { simple_slide.slider[index].next() },simple_slide.slider[index].delay );
+        }
+
+        if(simple_slide.slider[index].nb_of_slides > 1)
+            simple_slide.slider[index].timer = setInterval( function() { simple_slide.slider[index].next() },simple_slide.slider[index].delay );
+
     });
+    
 
-    simple_slide.next_slide = function()
-    {
-        if(this.index == this.slides.length-1)
-        {
-            this.slides.eq(0).css({'left':'0','z-index':'0'});
-        }
-        if(this.index == this.slides.length)
-        {
-            this.index = 0;
-            this.slides.css('left','0');
-            this.slides.eq(this.index).css('z-index', simple_slide.slides.length);
-        }
-        this.slides.eq(this.index).animate({"left":"-="+simple_slide.width+"px"},"slow");
-        this.index ++;
-    }
 
-    simple_slide.previous_slide = function()
-    {
-        if(this.index == this.slides.length)
-        {
-            this.slides.css({'left':'-'+simple_slide.width+"px"});
-            this.slides.eq(this.index).css('left','0');
-            this.slides.eq(0).css({'left':'0','z-index':'0'});
-        }
-        if(this.index == 1)
-        {
-            this.slides.eq(0).css('z-index', simple_slide.slides.length);
-            this.slides.eq(0).css({'left':'-'+simple_slide.width+"px"});
-        }
-        if(this.index == 0)
-        {
-            this.slides.css({'left':'-'+simple_slide.width+"px"});
-            this.slides.eq(this.index).css({'left':'0','z-index':'0'});
-            this.index = this.slides.length;
-        }
-        this.index --;
-        this.slides.eq(this.index).animate({"left":"+="+simple_slide.width+"px"},"slow");
-    }
 
-    simple_slide.previous = function()
-    {
-        clearInterval(this.timer);
-        this.previous_slide();
-        this.timer = setInterval( function() { simple_slide.next() },simple_slide.delay );
-    }
-
-    simple_slide.next = function()
-    {
-        clearInterval(this.timer);
-        this.next_slide();
-        this.timer = setInterval( function() { simple_slide.next() },simple_slide.delay );
-    }
-
-    if(simple_slide.slides.length > 1)
-        simple_slide.timer = setInterval( function() { simple_slide.next() },simple_slide.delay );
 
 });
